4ee5e6abd1f5764304886551327e9d4c218235ef
caucas <sdmagomedovmagomed@gmail.com>
2023-11-23 21:21:22 +0300

    вторая часть

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         int age = 2;
+        int age2 = 3;
         System.out.println(age);
+        System.out.println(age2);
     }
 }
