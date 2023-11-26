fc7f123144d97f457aabc121ba67488651407730
caucas <sdmagomedovmagomed@gmail.com>
2023-11-26 18:35:31 +0300

    Первая домашка (Итоговый вариант )

--- src/org/example/homework1.java
+++ src/org/example/homework1.java
@@ -9,16 +9,16 @@ public class homework1 {
     }
     public static void ex1() {
         String name = "     ПЕтРов Олег Иванович     ";
-        name.trim();
+        name = name.trim();
 
-        name.toUpperCase();
+        name = name.toUpperCase();
 
-        if (name.contains("ова")) {
+        if (name.contains("ОВА")) {
             System.out.printf("Уважаемая", name);
-        }else if (name.contains("ов")) {
+        }else if (name.contains("ОВ")) {
             System.out.printf("Уважаемый", name);
         }else {
-            System.out.printf("Неизвестное лицо");
+            System.out.println("Неизвестное лицо");
         }
     }
     public static void ex2(){
@@ -34,9 +34,7 @@ public class homework1 {
         boolean isWheelWork3 = true;
         boolean isWheelWork4 = true;
 
-        if (fuel >= 10 && (isWheelWork1 & isWheelWork2 & isWheelWork3 & isWheelWork4)
-                && !hasErrors & isEngineWork
-        ) {
+        if (fuel >= 10 && (isWheelWork1 & isWheelWork2 & isWheelWork3 & isWheelWork4) && !hasErrors & isEngineWork) {
             System.out.println("\nМашина работает");
         } else {
             System.out.println("\nМашина не работает");
@@ -45,6 +43,7 @@ public class homework1 {
     public static void ex3(){
 
         String simply = "this is simply. This is my favorite song.";
+
         simply = simply.replace("this is", "those are");
 
         simply = simply.replace("This is", "Those are");
@@ -53,62 +52,62 @@ public class homework1 {
         System.out.println(indexSimply);
     }
     public static void ex4() {
+        //Сколько произведение
         int sausageProduced = 2000;
         int hamProduced = 8511;
         int neckProduced = 6988;
-
+        //Входные данные, сколько продано
         int sausageSell = 2000;
         int hamSell = 8511;
         int neckSell = 6988;
-
+        //Цена продажи
         int sausagePrice = 800;
+        //Считаем себестоимость
         int costOfSausageProduction = 0;
-
         if (sausageProduced < 1000) {
             costOfSausageProduction = 412;
-        }
-
-        if (sausageProduced >= 1000 & sausageProduced < 2000) {
+        }else if (sausageProduced >= 1000 & sausageProduced < 2000) {
+            costOfSausageProduction = 404;
+        }else if (sausageProduced >= 2000) {
             costOfSausageProduction = 404;
         }
-        if (sausageProduced >= 2000) {
-            costOfSausageProduction = 404;
-        }
-
+        //Ветчина - стоимость 350 руб
+        //себестоимость при производстве - 275 руб
         int hamPrice = 350;
         int costOfHamProduction = 275;
-
+        //Шейка - стоимость 500 руб
+        //Считаем себестоимость
         int neckPrice = 500;
         int costOfNeckProduction = 0;
-
         if (neckProduced < 500) {
             costOfNeckProduction = 412;
-        }
-        if (neckProduced >= 500) {
+        }else if (neckProduced >= 500) {
             costOfNeckProduction = 290;
         }
         //Финансовые показатели
+        //Доход компании
         int incomeForSausage = sausagePrice * sausageSell;
         int incomeForHam = hamPrice * hamSell;
         int incomeForNeck = neckPrice * neckSell;
         int incomeCompany = incomeForNeck + incomeForHam + incomeForNeck;
-
+        //Расход компании
         int consumptionForSausage = costOfSausageProduction * sausageSell;
         int consumptionForHam = costOfHamProduction * hamSell;
         int consumptionForNeck = costOfNeckProduction * neckSell;
         int consumptionForCompany = consumptionForSausage + consumptionForHam + consumptionForNeck + 1_000_000;
-
+        //Прибыль до налогов
         int profitBeforeTaxes = incomeCompany - consumptionForCompany;
-
-        double companyProfitWithTax = 0;
+        //Считаем сколько налогов платим
+        double incomeTax = 0;
         if (incomeCompany > 2_000_000) {
-            companyProfitWithTax = incomeCompany * 0.13;
-        }if (incomeCompany > 1_000_000 & incomeCompany <2_000_000) {
-            companyProfitWithTax = incomeCompany * 0.1;
-        }if (incomeCompany < 1_000_000) {
-            companyProfitWithTax = incomeCompany * 0.08;
+            incomeTax = incomeCompany * 0.13;
+        }else if (incomeCompany > 1_000_000 & incomeCompany <2_000_000) {
+            incomeTax = incomeCompany * 0.1;
+        }else if (incomeCompany < 1_000_000) {
+            incomeTax = incomeCompany * 0.08;
         }
-        double profitAfterTaxes = profitBeforeTaxes - companyProfitWithTax;
+        //Прибыль после вычета налогов
+        double profitAfterTaxes = profitBeforeTaxes - incomeTax;
         System.out.println(profitAfterTaxes);
     }
 
